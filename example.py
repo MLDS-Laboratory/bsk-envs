8380cc8954cc0e4c85373f69413d75a14a61b3bb
aarunsrinivas5 <aarun5@umd.edu>
2025-03-19 17:03:18 -0500

    Removed stable baselines (#1)

--- example.py
+++ example.py
@@ -1,18 +1,14 @@
 import numpy as np
 import gymnasium as gym
 import bsk_envs
-from stable_baselines3 import PPO
-
 
 env = gym.make('OrbitDiscovery3DOF-v0', render_mode='human')
-model = PPO.load(f'baselines/orbit_discovery-medium', env)
 
 done = False
 state, _ = env.reset()
 total_reward = 0
 while not done:
-    action, _ = model.predict(state, deterministic=True)
-    state, reward, done, _, info = env.step(action)
+    state, reward, done, _, info = env.step(env.action_space.sample())
     total_reward += reward
     env.render()
 env.close()
